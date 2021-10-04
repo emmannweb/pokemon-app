ca4e94b85a94427106a7f3a817471288688e2fc5
Manuel <emmanuel.francois@onexsolucoes.com.br>
2021-10-04 16:45:39 -0300

    Adding Github link in footer

--- src/components/common_files/Footer.js
+++ src/components/common_files/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <>
            <div className="text-center p-4 footer" style={{backgroundColor: '#fafafa'}}>
-                © 2021 Copyright: <a className="text-reset fw-bold" href="#">POKÉDEX</a>    
+                © 2021 Copyright: <a className="text-reset fw-bold" target="_blank" rel="noreferrer" href="https://github.com/emmannweb/pokemon-app.git">POKÉDEX</a>    
             </div>
         </>
     )
